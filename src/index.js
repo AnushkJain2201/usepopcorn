66c2c51825d60bf882dcaaace34863081756d894
Anushk Jain <anushk2201@gmail.com>
2023-12-20 00:02:57 +0530

    feat:improved reusability using props

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
 // import './index.css';
 
 import StarRating from './StarRating';
 
+const Test = () => {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color='blue' maxRating={10} onSetRating = {setMovieRating} />
+
+      <p>This Movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={8} />
+    <StarRating maxRating={5} defaultRating = {3} />
+
+    <Test />
   </React.StrictMode>
 );
 
