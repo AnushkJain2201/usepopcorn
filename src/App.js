9c34f6e6e3dffd47b74b7c887bd3c718b199b8ef
Anushk Jain <anushk2201@gmail.com>
2023-12-28 19:48:53 +0530

    feat: used useEffect hook.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -55,7 +55,13 @@ export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
 
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`).then(res => res.json()).then(data => console.log(data));
+  // fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`).then(res => res.json()).then(data => console.log(data));
+
+  useEffect(() => {
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+    .then(res => res.json())
+    .then(data => setMovies(data.Search))
+  }, [])
  
   return (
     <>
